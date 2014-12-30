90a7f9664aa09f887b0dcb5d1e2ecd5aed496348
king <king@kingdeMacBook-Pro.local>
2014-12-30 23:54:15 +0800

    Initial Commit

---
+++ 二维码生成和扫描/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  二维码生成和扫描
+//
+//  Created by king on 14/12/30.
+//  Copyright (c) 2014年 East. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 二维码生成和扫描/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  二维码生成和扫描
+//
+//  Created by king on 14/12/30.
+//  Copyright (c) 2014年 East. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
